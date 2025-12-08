d6cf5ad2acec6d4ac66bfe3fbb6d44dd2a99f665
Steve Song <steve.b.song92@gmail.com>
2025-12-08 01:28:57 -0500

    feat: reorder projects and link to GitHub Pages
    - Order: WWF, Leica, Kakao T, Pokemon
    - Links now point to superkookoo.github.io project pages

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,20 +9,20 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    id: 'leica',
-    title: 'Leica',
-    category: 'Responsive Website',
-    description: 'Leica의 브랜드 정체성을 강화하는 방향으로 웹사이트를 리디자인했습니다.',
-    image: '/images/leica_main.jpg',
-    link: '/project/leica',
-  },
-  {
     id: 'wwf',
     title: 'WWF',
     category: 'Website Redesign',
     description: 'WWF 웹사이트 리디자인뿐 아니라, static으로 직접 구현까지 완료했습니다. 코딩 결과물은 오른쪽 하단 "View" 버튼을 통해 확인하실 수 있습니다.',
     image: '/images/wwf_site_mockup.jpg',
-    link: '/project/wwf',
+    link: 'https://superkookoo.github.io/portfolio/project/project_wwf.html',
+  },
+  {
+    id: 'leica',
+    title: 'Leica',
+    category: 'Responsive Website',
+    description: 'Leica의 브랜드 정체성을 강화하는 방향으로 웹사이트를 리디자인했습니다.',
+    image: '/images/leica_main.jpg',
+    link: 'https://superkookoo.github.io/portfolio/project/project_leica.html',
   },
   {
     id: 'kakao',
@@ -30,7 +30,7 @@ export const projects: Project[] = [
     category: 'Mobile Application Redesign',
     description: 'Kakao T 앱에서 택시 호출 기능만을 별도로 분리해, 핵심 사용자 흐름에 집중한 UI 중심으로 리디자인하였습니다.',
     image: '/images/kakao_splash.jpg',
-    link: '/project/kakao',
+    link: 'https://superkookoo.github.io/portfolio/project/project_kakao.html',
   },
   {
     id: 'pokemon',
@@ -38,6 +38,6 @@ export const projects: Project[] = [
     category: 'Promotion',
     description: '기존의 롯데월드 X 포켓몬스터 콜라보레이션을 새로운 시각으로 재해석하여 프로모션 페이지를 구성했습니다. 전체 톤앤매너에 맞춰 페이지 하단에는 색감을 고려한 트래블 포스터를 배치해 시각적 완성도를 더했습니다.',
     image: '/images/lotte_pokemon.jpg',
-    link: '/project/pokemon',
+    link: 'https://superkookoo.github.io/portfolio/project/project_pokemon.html',
   },
 ]
